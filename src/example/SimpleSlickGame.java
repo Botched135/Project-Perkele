fd5f3a7cb41e5f232a2b2d733acb8bc02859bd83
Hethix <An.ryge@gmail.com>
2015-03-18 13:46:44 +0100

    Made the SimpleSlickGame into our main and stategolder

--- src/example/SimpleSlickGame.java
+++ src/example/SimpleSlickGame.java
@@ -1,29 +1,20 @@
 package example;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
 
-public class SimpleSlickGame extends BasicGame
+public class SimpleSlickGame extends StateBasedGame
 {
 	public SimpleSlickGame(String gamename)
 	{
 		super(gamename);
-	}
-
-	@Override
-	public void init(GameContainer gc) throws SlickException {}
-
-	@Override
-	public void update(GameContainer gc, int i) throws SlickException {}
-
-	@Override
-	public void render(GameContainer gc, Graphics g) throws SlickException
-	{
-		g.drawString("Welcome to Project Perkele!", 500, 300);
 	}
 
 	public static void main(String[] args)
@@ -32,12 +23,16 @@ public class SimpleSlickGame extends BasicGame
 		{
 			AppGameContainer appgc;
 			appgc = new AppGameContainer(new SimpleSlickGame("Simple Slick Game"));
-			appgc.setDisplayMode(1280, 720, true);
+			appgc.setDisplayMode(1280, 720, false);
 			appgc.start();
 		}
 		catch (SlickException ex)
 		{
 			Logger.getLogger(SimpleSlickGame.class.getName()).log(Level.SEVERE, null, ex);
 		}
+	}
+
+	public void initStatesList(GameContainer gc) throws SlickException {
+		this.addState(new GameState());
 	}
 }

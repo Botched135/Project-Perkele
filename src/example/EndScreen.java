90b2de6d6f84be8332ab15bf54e460213b13d53a
Botched135 <rasmusbons@gmail.com>
2015-05-01 18:18:09 +0200

    EndScreen slightly updated

--- src/example/EndScreen.java
+++ src/example/EndScreen.java
@@ -15,7 +15,8 @@ public class EndScreen extends BasicGameState {
 	//sounds
 	public static Sound EndScreenTheme = null;
 	
-	//
+	//Other Variables
+	public static int wave;
 	
 	
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
@@ -38,8 +39,10 @@ public class EndScreen extends BasicGameState {
 		}
 	}
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)throws SlickException{
-		g.drawString("You died at Wave ", Window.WIDTH/2, Window.HEIGHT/2);
-		g.drawString("Press Enter or Mouse to return to main menu", Window.WIDTH/2,Window.HEIGHT/2+64);
+		g.setColor(new Color(255,0,0));
+		
+		g.drawString("YOU DIED AT WAVE "+EndScreen.wave, (Window.WIDTH/2)-86, Window.HEIGHT/2-64);
+		g.drawString("Press Enter or Mouse to return to main menu", Window.WIDTH/2-190,Window.HEIGHT/2+64);
 	}
 	public int getID(){
 		return 2; //ID of this state
